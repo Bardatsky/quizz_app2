b8a48b36a3dfc1fb9730fbb1fd547f6a1a65da9b
Егор Бардацкиий <yegor_bardatsky@MacBook-Air-Egor.local>
2023-12-27 15:49:59 +0300

    fixed issue

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app2/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
