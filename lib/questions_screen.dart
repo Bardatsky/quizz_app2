ee2530642644d7901c3117e65cb18d6288f0fa80
Егор Бардацкиий <yegor_bardatsky@MacBook-Air-Egor.local>
2024-01-01 22:26:34 +0300

    lesson 67

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('questions screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column( 
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text('question...', style: TextStyle(color: Colors.white),),
+        const SizedBox(height: 30,),
+        ElevatedButton(onPressed: () {}, child: const Text('answer 1')),
+        const SizedBox(height: 10,),
+        ElevatedButton(onPressed: () {}, child: const Text('answer 2')),
+        const SizedBox(height: 10,),
+        ElevatedButton(onPressed: () {}, child: const Text('answer 3')),
+        const SizedBox(height: 10,),
+        ElevatedButton(onPressed: () {}, child: const Text('answer 4')),
+        
+      ],
+      
+      ),
+    );
+    
   }
 }
